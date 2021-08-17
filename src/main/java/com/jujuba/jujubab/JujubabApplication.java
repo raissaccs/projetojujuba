6f7336cd8e83f6253d054707e5bfb8c963836966
Raissa Cristina <raissacristinac@gmail.com>
2021-08-17 20:14:02 -0300

    iniciando o projeto

---
+++ src/main/java/com/jujuba/jujubab/JujubabApplication.java
@@ -0,0 +1,13 @@
+package com.jujuba.jujubab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JujubabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JujubabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jujuba/jujubab/JujubabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jujuba.jujubab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JujubabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
